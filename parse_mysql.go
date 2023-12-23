9d69ee87111f672bde3390fd9785056e0a95722a
kodaimura <kodaimura123@gmail.com>
2023-12-23 23:58:10 +0900

    outline

--- parse_mysql.go
+++ parse_mysql.go
@@ -1,5 +1,8 @@
 package ddlparse
 
+import (
+	"errors"
+)
 
 type mysqlParser struct {
 	tokens []string
@@ -17,12 +20,64 @@ func (p *mysqlParser) init() {
 	p.lines = 0
 }
 
-func (p *mysqlParser) next() {
+func (p *mysqlParser) next() error {
 	p.i += 1
-	if (p.i <= p.size && p.tokens[p.i] == "\n") {
-		p.lines += 1
-		p.next()
+	if (p.i > p.size) {
+		return nil;
 	}
+	if (p.tokens[p.i] == "\n") {
+		p.lines += 1
+		return p.next()
+	} else if (p.tokens[p.i] == "--") {
+		p.skipSingleLineComment()
+		return p.next()
+	} else if (p.tokens[p.i] == "/*") {
+		if err := p.skipMultiLineComment(); err != nil {
+			return err
+		}
+		return p.next()
+	} else {
+		return nil
+	}
+}
+
+func (p *mysqlParser) skipSingleLineComment() {
+	if (p.tokens[p.i] != "--") {
+		return
+	}
+	var skip func()
+	skip = func() {
+		p.i += 1
+		if (p.i > p.size) {
+			return
+		} else if (p.tokens[p.i] == "\n") {
+			p.lines += 1
+		} else {
+			skip()
+		}
+	}
+	skip()
+}
+
+func (p *mysqlParser) skipMultiLineComment() error {
+	if (p.tokens[p.i] != "/*") {
+		return nil
+	}
+	var skip func() error
+	skip = func() error {
+		p.i += 1
+		if (p.i > p.size) {
+			return errors.New("expected '*/' but not found")
+		} else if (p.tokens[p.i] == "\n") {
+			p.lines += 1
+			return skip()
+		} else if (p.tokens[p.i] == "*/") {
+			return nil
+		} else {
+			return skip()
+		}
+	}
+	return skip()
 }
 
 func (p *mysqlParser) Validate() error {
--- parse_postgresql.go
+++ parse_postgresql.go
@@ -1,5 +1,8 @@
 package ddlparse
 
+import (
+	"errors"
+)
 
 type postgresqlParser struct {
 	tokens []string
@@ -17,12 +20,64 @@ func (p *postgresqlParser) init() {
 	p.lines = 0
 }
 
-func (p *postgresqlParser) next() {
+func (p *postgresqlParser) next() error {
 	p.i += 1
-	if (p.i <= p.size && p.tokens[p.i] == "\n") {
-		p.lines += 1
-		p.next()
+	if (p.i > p.size) {
+		return nil;
 	}
+	if (p.tokens[p.i] == "\n") {
+		p.lines += 1
+		return p.next()
+	} else if (p.tokens[p.i] == "--") {
+		p.skipSingleLineComment()
+		return p.next()
+	} else if (p.tokens[p.i] == "/*") {
+		if err := p.skipMultiLineComment(); err != nil {
+			return err
+		}
+		return p.next()
+	} else {
+		return nil
+	}
+}
+
+func (p *postgresqlParser) skipSingleLineComment() {
+	if (p.tokens[p.i] != "--") {
+		return
+	}
+	var skip func()
+	skip = func() {
+		p.i += 1
+		if (p.i > p.size) {
+			return
+		} else if (p.tokens[p.i] == "\n") {
+			p.lines += 1
+		} else {
+			skip()
+		}
+	}
+	skip()
+}
+
+func (p *postgresqlParser) skipMultiLineComment() error {
+	if (p.tokens[p.i] != "/*") {
+		return nil
+	}
+	var skip func() error
+	skip = func() error {
+		p.i += 1
+		if (p.i > p.size) {
+			return errors.New("expected '*/' but not found")
+		} else if (p.tokens[p.i] == "\n") {
+			p.lines += 1
+			return skip()
+		} else if (p.tokens[p.i] == "*/") {
+			return nil
+		} else {
+			return skip()
+		}
+	}
+	return skip()
 }
 
 func (p *postgresqlParser) Validate() error {
