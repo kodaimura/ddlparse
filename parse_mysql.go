bb2ecbd7c5d878cc4bbda1c731964f56675aeecd
kodaimura <kodaimura123@gmail.com>
2024-01-19 23:40:45 +0900

    init parse_mysql.go

--- parse_mysql.go
+++ parse_mysql.go
@@ -8,7 +8,6 @@ import (
 
 type mysqlParser struct {
 	ddl string
-	ddlr []rune
 	tokens []string
 	validatedTokens []string
 	size int
@@ -22,13 +21,136 @@ func newMySQLParser(ddl string) parser {
 	return &mysqlParser{ddl: ddl, ddlr: []rune(ddl)}
 }
 
+
 func (p *mysqlParser) token() string {
 	return p.tokens[p.i]
 }
 
+
+func (p *mysqlParser) appendToken(token string) {
+	if (token != "") {
+		p.tokens = append(p.tokens, token)
+	}
+}
+
+
 func (p *mysqlParser) isOutOfRange() bool {
 	return p.i > p.size - 1
 }
+
+
+/*
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+  VALIDATE: 
+    Check the syntax of DDL (tokens). 
+    And eliminate unnecessary tokens during parsing.
+	Return an ValidateError if validation fails.
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+
+Example:
+
+***** tokens *****
+[CREATE TABLE IF NOT users ( \n id INTEGER PRIMARY KEY AUTOINCREMENT , \n
+ name TEXT NOT NULL , \n password TEXT NOT NULL , \n created_at TEXT NOT NULL 
+ DEFAULT ( DATETIME ( 'now' , 'localtime' ) ) , \n updated_at TEXT NOT NULL 
+ DEFAULT ( DATETIME ( 'now' , 'localtime' ) ) , \n UNIQUE ( name ) \n ) ;]
+
+***** validatedTokens *****
+[CREATE TABLE users ( \n id INTEGER PRIMARY KEY AUTOINCREMENT , \n
+ name TEXT NOT NULL , \n password TEXT NOT NULL , \n created_at TEXT NOT NULL 
+ DEFAULT ( DATETIME ( 'now' , 'localtime' ) ) , \n updated_at TEXT NOT NULL 
+ DEFAULT ( DATETIME ( 'now' , 'localtime' ) ) , \n UNIQUE ( name ) \n ) ;]
+
+////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+*/
+
+func (p *mysqlParser) Validate() error {
+	tokens, err := Tokenize(p.ddl, SQLite)
+	if err != nil {
+		return err
+	}
+	p.tokens = tokens
+	return p.validate()
+}
+
+
+func (p *mysqlParser) validate() error {
+	p.initV()
+	return p.validateProc()
+}
+
+
+func (p *mysqlParser) initV() {
+	p.validatedTokens = []string{}
+	p.i = -1
+	p.line = 1
+	p.size = len(p.tokens)
+	p.flg = false
+	p.next()
+}
+
+
+func (p *mysqlParser) flgOn() {
+	p.flg = true
+}
+
+
+func (p *mysqlParser) flgOff() {
+	p.flg = false
+}
+
+
+func (p *mysqlParser) next() error {
+	if p.flg {
+		p.validatedTokens = append(p.validatedTokens, p.token())
+	}
+	return p.nextAux()
+}
+
+
+func (p *mysqlParser) nextAux() error {
+	p.i += 1
+	if (p.isOutOfRange()) {
+		return errors.New("out of range")
+	}
+	if (p.token() == "\n") {
+		p.line += 1
+		return p.nextAux()
+	} else {
+		return nil
+	}
+}
+
+
+func (p *mysqlParser) matchKeyword(keywords ...string) bool {
+	return contains(
+		append(
+			mapSlice(keywords, strings.ToLower), 
+			mapSlice(keywords, strings.ToUpper)...,
+		), p.token())
+}
+
+
+func (p *mysqlParser) matchSymbol(symbols ...string) bool {
+	return contains(symbols, p.token())
+}
+
+
+func (p *mysqlParser) validateProc() error {
+	if (p.isOutOfRange()) {
+		return nil
+	}
+	if err := p.validateCreateTable(); err != nil {
+		return err
+	}
+	return p.validateProc()
+}
+
 
 func (p *mysqlParser) syntaxError() error {
 	if p.isOutOfRange() {
@@ -37,90 +159,704 @@ func (p *mysqlParser) syntaxError() error {
 	return NewValidateError(p.line, p.tokens[p.i])
 }
 
-func (p *mysqlParser) init() {
-	p.i = -1
-	p.line = 1
-	p.next()
-}
 
-func (p *mysqlParser) next() error {
-	p.i += 1
+func (p *mysqlParser) validateKeyword(keywords ...string) error {
 	if (p.isOutOfRange()) {
-		return errors.New("out of range")
+		return p.syntaxError()
 	}
-	if (p.token() == "\n") {
-		p.line += 1
-		return p.next()
-	} else if (p.token() == "--") {
-		p.skipSingleLineComment()
-		return p.next()
-	} else if (p.token() == "/*") {
-		if err := p.skipMultiLineComment(); err != nil {
-			return err
+	if p.matchKeyword(keywords...) {
+		if p.next() != nil {
+			return p.syntaxError()
 		}
-		return p.next()
-	} else {
 		return nil
 	}
+	return p.syntaxError()
 }
 
-func (p *mysqlParser) skipSingleLineComment() {
-	if (p.token() != "--") {
-		return
+
+func (p *mysqlParser) validateSymbol(symbols ...string) error {
+	if (p.isOutOfRange()) {
+		return p.syntaxError()
 	}
-	var skip func()
-	skip = func() {
-		p.i += 1
-		if (p.isOutOfRange()) {
-			return
-		} else if (p.token() == "\n") {
-			p.line += 1
-		} else {
-			skip()
+	if p.matchSymbol(symbols...) {
+		if p.next() != nil {
+			return p.syntaxError()
 		}
-	}
-	skip()
-}
-
-func (p *mysqlParser) skipMultiLineComment() error {
-	if (p.token() != "/*") {
 		return nil
 	}
-	var skip func() error
-	skip = func() error {
-		p.i += 1
-		if (p.isOutOfRange()) {
-			return errors.New("out of range")
-		} else if (p.token() == "\n") {
-			p.line += 1
-			return skip()
-		} else if (p.token() == "*/") {
-			return nil
-		} else {
-			return skip()
-		}
-	}
-	return skip()
+	return p.syntaxError()
 }
+
 
 func (p *mysqlParser) isValidName(name string) bool {
-	pattern := regexp.MustCompile(`^[a-zA-Z0-9_]*$`)
+	pattern := regexp.MustCompile(`^[a-zA-Z_][a-zA-Z0-9_]*$`)
 	return pattern.MatchString(name) && 
-		!contains(ReservedWords_MySQL, strings.ToUpper(name))
+		!contains(ReservedWords_SQLite, strings.ToUpper(name))
 }
+
 
 func (p *mysqlParser) isValidQuotedName(name string) bool {
-	pattern := regexp.MustCompile(`^[a-zA-Z0-9_]*$`)
-	return pattern.MatchString(name)
+	return true
 }
 
-func (p *mysqlParser) Validate() error {
-	p.init()
+
+func (p *mysqlParser) validateName() error {
+	if isStringToken(p.token()) {
+		if !p.isValidQuotedName(p.token()) {
+			return p.syntaxError()
+		}
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+	} else {
+		if !p.isValidName(p.token()) {
+			return p.syntaxError()
+		}
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+	}
+
+	return nil
+}
+
+
+func (p *mysqlParser) validateCreateTable() error {
+	p.flgOn()
+	if err := p.validateKeyword("CREATE"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("TABLE"); err != nil {
+		return err
+	}
+
+	p.flgOff()
+	if p.validateKeyword("IF") == nil {
+		if err := p.validateKeyword("NOT"); err != nil {
+			return err
+		}
+		if err := p.validateKeyword("EXISTS"); err != nil {
+			return err
+		}
+	}
+
+	p.flgOn()
+	if err := p.validateTableName(); err != nil {
+		return err
+	}
+	if err := p.validateSymbol("("); err != nil {
+		return err
+	}
+	if err := p.validateColumns(); err != nil {
+		return err
+	}
+	if err := p.validateSymbol(")"); err != nil {
+		return err
+	}
+	if err := p.validateTableOptions(); err != nil {
+		return err
+	}
+	if (p.token() == ";") {
+		if p.next() != nil {
+			return nil
+		}
+	}
+
+	return p.validateCreateTable()
+}
+
+
+func (p *mysqlParser) validateTableName() error {
+	if err := p.validateName(); err != nil {
+		return err
+	}
+	if p.validateSymbol(".") == nil {
+		if err := p.validateName(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+
+func (p *mysqlParser) validateColumns() error {
+	if err := p.validateColumn(); err != nil {
+		return err
+	}
+	if p.validateSymbol(",") == nil {
+		return p.validateColumns()
+	}
+
+	return nil
+}
+
+
+func (p *mysqlParser) validateColumn() error {
+	if p.matchKeyword("CONSTRAINT", "PRIMARY", "UNIQUE", "CHECK", "FOREIGN") {
+		return p.validateTableConstraint()
+	}
+	if err := p.validateColumnName(); err != nil {
+		return err
+	}
+	if err := p.validateColumnType(); err != nil {
+		return err
+	}
+	if err := p.validateColumnConstraint(); err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+
+func (p *mysqlParser) validateColumnName() error {
+	return p.validateName()
+}
+
+
+// Omitting data types is not supported.
+func (p *mysqlParser) validateColumnType() error {
+	return p.validateKeyword("TEXT", "NUMERIC", "INTEGER", "REAL", "NONE")
+}
+
+
+func (p *mysqlParser) validateColumnConstraint() error {
+	p.flgOff()
+	if p.validateKeyword("CONSTRAINT") == nil {
+		if err := p.validateName(); err != nil {
+			return err
+		}
+	}
+	p.flgOn()
+	return p.validateColumnConstraintAux([]string{})
+}
+
+
+func (p *mysqlParser) validateColumnConstraintAux(ls []string) error {
+	if p.matchKeyword("PRIMARY") {
+		if contains(ls, "PRIMARY") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintPrimaryKey(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "PRIMARY"))
+	}
+
+	if p.matchKeyword("NOT") {
+		if contains(ls, "NOT") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintNotNull(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "NOT"))
+	}
+
+	if p.matchKeyword("UNIQUE") {
+		if contains(ls, "UNIQUE") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintUnique(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "UNIQUE"))
+	}
+
+	if p.matchKeyword("CHECK") {
+		if contains(ls, "CHECK") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintCheck(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "CHECK"))
+	}
+
+	if p.matchKeyword("DEFAULT") {
+		if contains(ls, "DEFAULT") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintDefault(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "DEFAULT"))
+	}
+
+	if p.matchKeyword("COLLATE") {
+		if contains(ls, "COLLATE") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintCollate(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "COLLATE"))
+	}
+
+	if p.matchKeyword("REFERENCES") {
+		if contains(ls, "REFERENCES") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintForeignKey(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "REFERENCES"))
+	}
+
+	if p.matchKeyword("GENERATED", "AS") {
+		if contains(ls, "GENERATED") {
+			return p.syntaxError()
+		}
+		if err := p.validateConstraintGenerated(); err != nil {
+			return err
+		}
+		return p.validateColumnConstraintAux(append(ls, "GENERATED"))
+	}
+
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintPrimaryKey() error {
+	p.flgOn()
+	if err := p.validateKeyword("PRIMARY"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("KEY"); err != nil {
+		return err
+	}
+	p.flgOff()
+	if p.matchKeyword("ASC", "DESC") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+	}
+	if err := p.validateConflictClause(); err != nil {
+		return err
+	}
+	p.flgOn()
+	if p.matchKeyword("AUTOINCREMENT") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+	}
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintNotNull() error {
+	p.flgOn()
+	if err := p.validateKeyword("NOT"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("NULL"); err != nil {
+		return err
+	}
+	p.flgOff()
+	if err := p.validateConflictClause(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintUnique() error {
+	p.flgOn()
+	if err := p.validateKeyword("UNIQUE"); err != nil {
+		return err
+	}
+	p.flgOff()
+	if err := p.validateConflictClause(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintCheck() error {
+	p.flgOff()
+	if err := p.validateKeyword("CHECK"); err != nil {
+		return err
+	}
+	if err := p.validateExpr(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintDefault() error {
+	p.flgOn()
+	if err := p.validateKeyword("DEFAULT"); err != nil {
+		return err
+	}
+	if p.matchSymbol("(") {
+		if err := p.validateExpr(); err != nil {
+			return err
+		}
+	} else {
+		if err := p.validateLiteralValue(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintCollate() error {
+	p.flgOff()
+	if err := p.validateKeyword("COLLATE"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("BINARY","NOCASE", "RTRIM"); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintForeignKey() error {
+	p.flgOff()
+	if err := p.validateKeyword("REFERENCES"); err != nil {
+		return err
+	}
+	if err := p.validateTableName(); err != nil {
+		return err
+	}
+	if p.validateSymbol("(") == nil {
+		if err := p.validateCommaSeparatedColumnNames(); err != nil {
+			return err
+		}
+		if err := p.validateSymbol(")"); err != nil {
+			return err
+		}
+	}
+	if err := p.validateConstraintForeignKeyAux(); err != nil {
+		return p.syntaxError()
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintForeignKeyAux() error {
+	p.flgOff()
+	if p.validateKeyword("ON") == nil {
+		if err := p.validateKeyword("DELETE", "UPDATE"); err != nil {
+			return err
+		}
+		if p.validateKeyword("SET") == nil {
+			if err := p.validateKeyword("NULL", "DEFAULT"); err != nil {
+				return err
+			}
+		} else if p.validateKeyword("CASCADE", "RESTRICT") == nil {
+
+		} else if p.validateKeyword("NO") == nil {
+			if err := p.validateKeyword("ACTION"); err != nil {
+				return err
+			}
+		} else {
+			return p.syntaxError()
+		}
+		return p.validateConstraintForeignKeyAux()
+	}
+
+	if p.validateKeyword("MATCH") == nil {
+		if err := p.validateKeyword("SIMPLE", "PARTIAL", "FULL"); err != nil {
+			return err
+		}
+		return p.validateConstraintForeignKeyAux()
+	}
+
+	if p.matchKeyword("NOT", "DEFERRABLE") {
+		if p.matchKeyword("NOT") {
+			if p.next() != nil {
+				return p.syntaxError()
+			}
+		}
+		if err := p.validateKeyword("DEFERRABLE"); err != nil {
+			return err
+		}
+		if p.validateKeyword("INITIALLY") == nil {
+			if err := p.validateKeyword("DEFERRED", "IMMEDIATE"); err != nil {
+				return err
+			}
+		}
+		return p.validateConstraintForeignKeyAux()
+	}
+
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConstraintGenerated() error {
+	p.flgOff()
+	if p.validateKeyword("GENERATED") == nil {
+		if err := p.validateKeyword("ALWAYS"); err != nil {
+			return err
+		}
+	}
+	if err := p.validateKeyword("AS"); err != nil {
+		return err
+	}
+	if err := p.validateExpr(); err != nil {
+		return err
+	}
+	if p.matchKeyword("STORED", "VIRTUAL") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateConflictClause() error {
+	p.flgOff()
+	if p.validateKeyword("ON") == nil {
+		if err := p.validateKeyword("CONFLICT"); err != nil {
+			return err
+		}
+		if err := p.validateKeyword("ROLLBACK", "ABORT", "FAIL", "IGNORE","REPLACE"); err != nil {
+			return err
+		}
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateExpr() error {
+	if err := p.validateSymbol("("); err != nil {
+		return err
+	}
+	if err := p.validateExprAux(); err != nil {
+		return err
+	}
+	if err := p.validateSymbol(")"); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (p *mysqlParser) validateExprAux() error {
+	if p.matchSymbol(")") {
+		return nil
+	}
+	if p.matchSymbol("(") {
+		if err := p.validateExpr(); err != nil {
+			return err
+		}
+		return p.validateExprAux()
+	}
+	if p.next() != nil {
+		return p.syntaxError()
+	}
+	return p.validateExprAux()
+}
+
+
+func (p *mysqlParser) validateLiteralValue() error {
+	if isNumericToken(p.token()) {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+		return nil
+	}
+	if isStringToken(p.token()) {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+		return nil
+	}
+	ls := []string{"NULL", "TRUE", "FALSE", "CURRENT_TIME", "CURRENT_DATE", "CURRENT_TIMESTAMP"}
+	if err := p.validateKeyword(ls...); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func (p *mysqlParser) validateTableConstraint() error {
+	p.flgOff()
+	if p.validateKeyword("CONSTRAINT") == nil{
+		if err := p.validateName(); err != nil {
+			return err
+		}
+	}
+	p.flgOn()
+	return p.validateTableConstraintAux()
+}
+
+
+func (p *mysqlParser) validateTableConstraintAux() error {
+	if p.matchKeyword("PRIMARY") {
+		return p.validateTablePrimaryKey()
+	}
+
+	if p.matchKeyword("UNIQUE") {
+		return p.validateTableUnique()
+	}
+
+	if p.matchKeyword("CHECK") {
+		return p.validateTableCheck()
+	}
+
+	if p.matchKeyword("FOREIGN") {
+		return p.validateTableForeignKey()
+	}
+
+	return p.syntaxError()
+}
+
+
+func (p *mysqlParser) validateTablePrimaryKey() error {
+	p.flgOn()
+	if err := p.validateKeyword("PRIMARY"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("KEY"); err != nil {
+		return err
+	}
+	if err := p.validateSymbol("("); err != nil {
+		return err
+	}
+	if err := p.validateCommaSeparatedColumnNames(); err != nil {
+		return p.syntaxError()
+	}
+	if err := p.validateSymbol(")"); err != nil {
+		return err
+	}
+	p.flgOff()
+	if err := p.validateConflictClause(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateTableUnique() error {
+	p.flgOn()
+	if err := p.validateKeyword("UNIQUE"); err != nil {
+		return err
+	}
+	if err := p.validateSymbol("("); err != nil {
+		return err
+	}
+	if err := p.validateCommaSeparatedColumnNames(); err != nil {
+		return p.syntaxError()
+	}
+	if err := p.validateSymbol(")"); err != nil {
+		return err
+	}
+	p.flgOff()
+	if err := p.validateConflictClause(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateTableCheck() error {
+	p.flgOff()
+	if err := p.validateKeyword("CHECK"); err != nil {
+		return err
+	}
+	if err := p.validateExpr(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateTableForeignKey() error {
+	p.flgOff()
+	if err := p.validateKeyword("FOREIGN"); err != nil {
+		return err
+	}
+	if err := p.validateKeyword("KEY"); err != nil {
+		return err
+	}
+	if err := p.validateSymbol("("); err != nil {
+		return err
+	}
+	if err := p.validateCommaSeparatedColumnNames(); err != nil {
+		return p.syntaxError()
+	}
+	if err := p.validateSymbol(")"); err != nil {
+		return err
+	}
+	if err := p.validateConstraintForeignKey(); err != nil {
+		return err
+	}
+	p.flgOn()
+	return nil
+}
+
+
+func (p *mysqlParser) validateCommaSeparatedColumnNames() error {
+	if err := p.validateColumnName(); err != nil {
+		return err
+	}
+	if p.matchSymbol(",") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+		return p.validateCommaSeparatedColumnNames()
+	}
+	return nil
+}
+
+
+func (p *mysqlParser) validateTableOptions() error {
+	p.flgOff()
+	if p.matchKeyword("WITHOUT") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+		if err := p.validateKeyword("ROWID"); err != nil {
+			return err
+		}
+		if p.matchSymbol(",") {
+			if p.next() != nil {
+				return p.syntaxError()
+			}
+			if err := p.validateKeyword("STRICT"); err != nil {
+				return err
+			}
+		}
+	} else if p.matchKeyword("STRICT") {
+		if p.next() != nil {
+			return p.syntaxError()
+		}
+		if p.matchSymbol(",") {
+			if p.next() != nil {
+				return p.syntaxError()
+			}
+			if err := p.validateKeyword("WITHOUT"); err != nil {
+				return err
+			}
+			if err := p.validateKeyword("ROWID"); err != nil {
+				return err
+			}
+		}
+	}
+	p.flgOn()
 	return nil
 }
 
 func (p *mysqlParser) Parse() ([]Table, error) {
-	p.init()
 	var tables []Table
 	return tables, nil
 }
